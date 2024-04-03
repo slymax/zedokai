581e3bd11bdaac244a578d207ec725080c76a847
Max <contact@slymax.com>
2024-04-04 00:35:39 +0200

    add modifications

--- languages/javascript/injections.scm
+++ languages/javascript/injections.scm
@@ -1,5 +1,9 @@
+(call_expression
+  (identifier) @language
+  (template_string) @content)
+
 ((comment) @content
   (#set! "language" "jsdoc"))
-  
+
 ((regex) @content
   (#set! "language" "regex"))
