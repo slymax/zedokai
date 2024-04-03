6de1020781f37127b165ecfc511834f70d1775c0
Max <contact@slymax.com>
2024-04-04 00:24:11 +0200

    add more languages

---
+++ languages/javascript/overrides.scm
@@ -0,0 +1,18 @@
+(comment) @comment
+
+[
+  (string)
+  (template_string)
+] @string
+
+[
+  (jsx_element)
+  (jsx_fragment)
+] @element
+
+[
+  (jsx_opening_element)
+  (jsx_closing_element)
+  (jsx_self_closing_element)
+  (jsx_expression)
+] @default
