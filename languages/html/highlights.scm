c4a0b3f9f7f10b85850f03e3946e071ad535f494
Max <contact@slymax.com>
2024-04-04 00:06:11 +0200

    add html

---
+++ languages/html/highlights.scm
@@ -0,0 +1,17 @@
+(tag_name) @keyword
+(erroneous_end_tag_name) @keyword
+(doctype) @constant
+(attribute_name) @attribute
+(attribute_value) @string
+(comment) @comment
+
+"=" @operator
+"\"" @punctuation.delimiter
+
+[
+  "<"
+  ">"
+  "<!"
+  "</"
+  "/>"
+] @punctuation.bracket
